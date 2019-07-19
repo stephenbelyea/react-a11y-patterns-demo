a09bbe099296f71d803c541022bbd197140978d0
Stephen Belyea <belyea.stephen@gmail.com>
2019-07-19 15:42:34 -0400

    Install and sample react-a11y-patterns as package

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button} from 'react-a11y-patterns'
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Button!</Button>
       </header>
     </div>
   );
